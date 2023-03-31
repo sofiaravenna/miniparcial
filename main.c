8e5d793d21e4fecfd99e24be9d233f42bfcee498
Sofía <ravennasofia03@gmail.com>
2023-03-31 19:18:19 -0300

    first commit

--- main.c
+++ main.c
@@ -1,25 +1,5 @@
 #include <stdio.h>
-
 int main() {
-
-    float nota, total=0, promedio;
-    int cantidad=0;
-
-    printf("El programa calculara el promedio de las notas ingresadas.\nPara finalizar ingrese -1.\n.\n");
-
-    do{
-        printf("Ingresar las notas: ");
-        scanf("%f",&nota);
-
-        if(nota != -1){
-            total=total+nota;
-            cantidad=cantidad+1;
-        }
-
-    }
-    while(nota != -1);
-    promedio=total/(cantidad);
-    printf("El promedio es: %f",promedio);
-
+    printf("Hola mundo");
     return 0;
 }
